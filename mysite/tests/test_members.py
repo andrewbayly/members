3623b08ef88c85d5cded6d1c5bf21d0ef2cd2181
Andrew Bayly <andrew.bayly@mac.com>
2024-03-29 16:20:13 -0700

    updated tests

--- mysite/tests/test_members.py
+++ mysite/tests/test_members.py
@@ -1,3 +1,16 @@
+"""
+Pre-requisite: 
+
+To make the tests work, there needs to be a single team member in the database ( with id=1 )
+This member cannot be edited.
+
+The following Test data is fine: 
+
+  Andrew Bayly (admin)
+  415-640-2710
+  andrew.bayly@mac.com
+
+"""
 
 def test_add_invalid_phone(py):   
     # visit the main page:
@@ -58,4 +71,48 @@ def test_add_invalid_email_invalid_domain_name(py):
     
    
     
+def test_add_duplicate_email(py):   
+    #first, setup a member
     
+    # visit the main page:
+    py.visit('http://localhost:8000')
+
+    # click the Add Button:
+    py.get("#addButton").click()
+
+    py.get("#id_FirstName").type("A")
+    py.get("#id_LastName").type("B")
+    py.get("#id_Email").type("a.b@mac.com")
+    py.get("#id_Phone").type("415-555-1234")
+    
+    py.get("#saveButton").click()
+    
+    #now, try to create another member with the same email
+
+    py.visit('http://localhost:8000')
+
+    # click the Add Button:
+    py.get("#addButton").click()
+
+    py.get("#id_FirstName").type("C")
+    py.get("#id_LastName").type("D")
+    py.get("#id_Email").type("a.b@mac.com")
+    py.get("#id_Phone").type("415-555-5678")
+    
+    py.get("#saveButton").click()
+    
+    assert py.contains('Another Team Member has the same Email Address')
+    
+    #clean-up: 
+    # go back and delete the created team member
+    
+    py.visit('http://localhost:8000')
+    
+    py.contains("A B").click()
+    
+    py.get("#deleteButton").click()
+    
+  
+  
+  
+  
